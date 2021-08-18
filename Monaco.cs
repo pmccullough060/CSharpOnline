811e7da06cfcca500b3b50558642b8808babbad4
Peter McCullough <peter.mccullough@atkinsglobal.com>
2021-08-18 20:43:17 +0100

    first commit

---
+++ Monaco.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSharpOnline
+{
+    public class Monaco
+    {
+        private readonly IJSInProcessRuntime runtime;
+
+        public Monaco(IJSRuntime runtime)
+        {
+            runtime = runtime as IJSInProcessRuntime;
+        }
+
+        public void Initialize(string elementId, string initialCode, string language) => runtime.Invoke<object>("monacoInterop.initialize", elementId, initialCode, language);
+
+        public string GetCode(string elementId) => runtime.Invoke<string>("monacoInterop.getCode", elementId);
+
+        public void SetCode(string elementId, string code) => runtime.Invoke<object>("monacoInterop.setCode", elementId, code);
+    }
+}
