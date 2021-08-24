21dbb67145f306f03d89d944210c8ca71c98b800
Peter McCullough <peter.mccullough@atkinsglobal.com>
2021-08-24 22:22:45 +0100

    Commit before pulling repo on other laptop

---
+++ Compiler.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSharpOnline
+{
+    public class Compiler
+    {
+        class BlazorBoot
+        {
+            public bool cacheBootResources { get; set; }
+            public object[] config { get; set; }
+            public bool debugBuild { get; set; }
+            public string entryAssembly { get; set; }
+            public bool linkerEnabled { get; set; }
+            public Resources resources { get; set; }
+        }
+
+        class Resources
+        {
+            public Dictionary<string, string> assembly { get; set; }
+            public Dictionary<string, string> pdb { get; set; }
+            public Dictionary<string, string> runtime { get; set; }
+        }
+
+        private static Task InitializationTask;
+        private static List<MetadataReference> References;
+
+    }
+}
